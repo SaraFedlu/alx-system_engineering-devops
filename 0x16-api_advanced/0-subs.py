d580b1d5a33f59803edb8f35f177d8addb328ced
Sara Fedlu <sfedlu28@gmail.com>
2024-01-31 05:24:19 -0800

    correct pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,17 +2,18 @@
 ''' Extract number of subscribers on reddit'''
 import requests
 
+
 def number_of_subscribers(subreddit):
     ''' Function for achieving the script purpose'''
     # Reddit API URL for getting subreddit information
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
+
     # Set a custom User-Agent header to avoid Too Many Requests error
     headers = {'User-Agent': 'linux/firefox-sarah'}
-    
+
     # Send GET request to Reddit API
     response = requests.get(url, headers=headers)
-    
+
     # Check if the request was successful
     if response.status_code == 200:
         # Parse JSON response
