be54680eb95db12718a581bcb25e1b62e1b24020
Sara Fedlu <sfedlu28@gmail.com>
2024-06-03 10:08:13 -0700

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+import csv
+import requests
+import sys
+
+
+def get_employee_todo_progress(employee_id):
+    try:
+        # Fetch employee data
+        user_response = requests.get(
+                f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+        user_response.raise_for_status()
+        user_data = user_response.json()
+
+        # Fetch todos data
+        todos_response = requests.get(
+            f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+        todos_response.raise_for_status()
+        todos_data = todos_response.json()
+
+        employee_name = user_data['name']
+        total_tasks = len(todos_data)
+        done_tasks = [task for task in todos_data if task['completed']]
+        number_of_done_tasks = len(done_tasks)
+
+        # Print the progress
+        print(f"Employee {employee_name} is done with tasks\
+({number_of_done_tasks}/{total_tasks}):")
+        for task in done_tasks:
+            print(f"\t {task['title']}")
+
+        # Export data to CSV
+        csv_filename = f"{employee_id}.csv"
+        with open(csv_filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                    ["USER_ID", "USERNAME",
+                        "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+            for task in todos_data:
+                writer.writerow(
+                        [employee_id,
+                            employee_name, task['completed'], task['title']])
+
+        print(f"Data exported to {csv_filename}")
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+    except KeyError as e:
+        print(f"Missing expected data: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
+    except ValueError:
+        print("The employee ID must be an integer.")
+        sys.exit(1)
