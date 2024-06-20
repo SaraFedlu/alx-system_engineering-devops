f6398388a9c96c0f987300ff0a92325459d2d709
Sara Fedlu <sfedlu28@gmail.com>
2024-06-19 23:21:26 -0700

    update the puppet manifest file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,28 @@
-# This Puppet manifest corrects a typo in the WordPress code
-file_line { 'fix-typo-class-wp-locale':
-  ensure => present,
-  path   => '/var/www/html/wp-settings.php',
-  match  => 'class-wp-locale.phpp',
-  line   => 'require_once ABSPATH . WPINC . \'/class-wp-locale.php\';',
-  notify => Service['apache2'],
+# Ensure the directory for Puppet manifests exists
+file { '/etc/puppet/manifests':
+  ensure => directory,
+}
+
+# Create the manifest to fix the typo in WordPress
+file { '/etc/puppet/manifests/fix-wp-locale.pp':
+  ensure  => file,
+  content => "exec { 'fix-typo-class-wp-locale':
+    command => 'sed -i \"s/class-wp-locale.phpp/class-wp-locale.php/\" /var/www/html/wp-settings.php',
+    path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  }",
+}
+
+# Apply the manifest file to fix the typo
+exec { 'apply-file-line':
+  command => 'puppet apply /etc/puppet/manifests/fix-wp-locale.pp',
+  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  notify  => Service['apache2'],
+  require => File['/etc/puppet/manifests/fix-wp-locale.pp'],
 }
 
 # Ensure Apache service is running and manage its state
 service { 'apache2':
-  ensure => running,
-  enable => true,
+  ensure  => running,
+  enable  => true,
+  require => Exec['apply-file-line'],
 }
